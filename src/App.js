a1a4c163d87a651b31474343cca4712783ce9ffc
Gurpreet Singh <gurpreet012guru@gmail.com>
2024-01-01 19:06:15 +0530

    newsletter page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import Team from './components/Team/Team';
 import Practices from './components/Practices/Practices';
 import FAQ from './components/FAQ/FAQ';
+import Newsletter from './components/Newsletter/Newsletter';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Clients />
       <Team />
       <FAQ />
+      <Newsletter />
     </>
   );
 }
